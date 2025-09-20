4559c83509ae2c49a0424ceb78caf4c55665e7fb
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-09-20 19:08:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	let fname = document.getElementById("fn").value;
+	let lname = document.getElementById("ln").value;
+
+	alert(`${fname} ${lname}`);
 
 }
+document.getElementById('submit').addEventListener('click', getFormvalue);
